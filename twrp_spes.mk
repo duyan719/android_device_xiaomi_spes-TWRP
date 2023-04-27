3d94863746e2e690819b8f9e0644f4a679d83c0f
Nguyễn Duy An <58502696+duyan719@users.noreply.github.com>
2023-04-27 01:57:11 -0700

    Update twrp_spes.mk

--- twrp_spes.mk
+++ twrp_spes.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
 BOARD_VENDOR := Redmi
